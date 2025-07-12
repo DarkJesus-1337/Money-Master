1e64f785b384c91ea7029d9ded074051b5e0b062
DarkJesus-1337 <s.dendtler@yahoo.com>
2025-07-12 17:00:12 +0200

    feat: New Colorscheme and Typography

--- app/src/main/java/com/pixelpioneer/moneymaster/ui/screens/dashboard/DashboardScreen.kt
+++ app/src/main/java/com/pixelpioneer/moneymaster/ui/screens/dashboard/DashboardScreen.kt
@@ -1,9 +1,6 @@
 package com.pixelpioneer.moneymaster.ui.screens.dashboard
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -13,22 +10,18 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.ArrowDownward
-import androidx.compose.material.icons.filled.ArrowUpward
-import androidx.compose.material.icons.filled.Balance
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
@@ -43,12 +36,8 @@ import com.pixelpioneer.moneymaster.ui.components.emptyview.EmptyTransactionsLis
 import com.pixelpioneer.moneymaster.ui.navigation.MoneyMasterBottomNavigation
 import com.pixelpioneer.moneymaster.ui.navigation.Screen
 import com.pixelpioneer.moneymaster.ui.viewmodel.BudgetViewModel
-import com.pixelpioneer.moneymaster.ui.viewmodel.FinancialSummary
 import com.pixelpioneer.moneymaster.ui.viewmodel.TransactionViewModel
-import com.pixelpioneer.moneymaster.util.FormatUtils
 import com.pixelpioneer.moneymaster.util.UiState
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.IconButton
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -62,6 +51,7 @@ fun DashboardScreen(
     val budgetsState = budgetViewModel.budgetsState.collectAsState().value
 
     Scaffold(
+        contentColor = MaterialTheme.colorScheme.onPrimaryContainer,
         topBar = {
             TopAppBar(
                 title = { Text("Dashboard") },
